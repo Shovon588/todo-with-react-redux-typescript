899e0fe7716ab5771829e9bc1439d32595abb8de
Shovon <mainulislam588@gmail.com>
2022-06-12 15:01:37 +0600

    Beautifying with tooltip

--- src/components/TodoList/TodoList/TodoItem.tsx
+++ src/components/TodoList/TodoList/TodoItem.tsx
@@ -1,4 +1,5 @@
 import { AiOutlineDelete } from "react-icons/ai";
+import ReactTooltip from "react-tooltip";
 
 interface TodoObjInterface {
     id: number;
@@ -11,23 +12,36 @@ interface Props {
 }
 
 const TodoItem: React.FC<Props> = ({ todoObject }) => {
-    const { title, completed } = todoObject;
+    const { id, title, completed } = todoObject;
 
     return (
         <div className="todo-list">
             <input type="text" readOnly className="input-field" value={title} />
 
-            <input
-                type="checkbox"
-                className="checkbox"
-                defaultChecked={completed}
-            />
+            <span
+                data-tip={`${
+                    id > 200
+                        ? "The mock api does not support update operation to newly created item. Please try with the default ones."
+                        : `Mark "${title}" as ${completed ? 'Todo' : 'Done'}`
+                }`}
+                data-for="toggleTip"
+            >
+                <input
+                    type="checkbox"
+                    className="checkbox"
+                    defaultChecked={completed}
+                    disabled={id > 200}
+                />
+            </span>
 
             <AiOutlineDelete
                 className="remove-icon"
-                data-tip="Deletes the todo from the list"
+                data-tip={`Delete "${title}" from the list`}
                 data-for="deleteTip"
             />
+
+            <ReactTooltip id="deleteTip" place="top" effect="solid" />
+            <ReactTooltip id="toggleTip" place="top" effect="solid" />
         </div>
     );
 };
